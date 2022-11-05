49568df9323c2c2572a7555cc30d4d19abe352e2
pedroygor <pedroygor2010@hotmail.com>
2022-11-05 04:24:46 -0300

    feat: post /users

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4, v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const user = {id: v4(), ...request.body, todos: []}
+  users.push(user)
+  return response.status(201).json({user})
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
